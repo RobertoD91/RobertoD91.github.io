2394d095b569f82ef5eb4760ae737632b243aa4b
Roberto <RobertoD91@users.noreply.github.com>
2022-12-19 01:29:02 +0100

    Create script.js

---
+++ static/test-os/script.js
@@ -0,0 +1,9 @@
+<script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+<script>
+  window.OneSignal = window.OneSignal || [];
+  OneSignal.push(function() {
+    OneSignal.init({
+      appId: "d811b3df-9da3-4127-8950-6290822fd37a",
+    });
+  });
+</script>
